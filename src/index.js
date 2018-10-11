023d3bccc251871b836d56847245154b291ce196
Aleksei Slesarev <lexsl84@gmail.com>
2018-10-11 17:27:01 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -12,9 +12,16 @@ class App extends React.Component {
         super(props);
         this.state = {
             breakLength: 5,
-            sessionLength: 25
+            sessionLength: 25,
+            mm: 25,
+            ss: '00',
+            timerLabel: 'Session',
+            play: false
         }
         this.clickControls = this.clickControls.bind(this);
+        this.startStop = this.startStop.bind(this);
+        this.reset = this.reset.bind(this);
+        this.seconds = this.seconds.bind(this);
     }
 
     clickControls(e) {
@@ -36,14 +43,16 @@ class App extends React.Component {
         } else if (e.target.value === 'sessionIncrement') {
             if (this.state.sessionLength < max) {
                 this.setState({
-                    sessionLength: this.state.sessionLength + 1
+                    sessionLength: this.state.sessionLength + 1,
+                    mm: this.state.mm + 1
                 })
             }
 
         } else if (e.target.value === 'sessionDecrement') {
             if (this.state.sessionLength > min) {
                 this.setState({
-                    sessionLength: this.state.sessionLength - 1
+                    sessionLength: this.state.sessionLength - 1,
+                    mm: this.state.mm - 1
                 })
             }
 
@@ -51,52 +60,71 @@ class App extends React.Component {
 
     }
 
-    render() {
-        return (<div id="pomodoro">
-            <div id="head">
-                <h1>Pomodoro clock</h1>
-            </div>
-            <div id="main">
-                <div id="settings">
-                    <div id="settings-head">
-                        <h3>Settings:</h3>
-                    </div>
-                    <div id="break-label">
-                        <div>
-                            <h3>Break</h3>
-                        </div>
-                        <div id="break-controls">
-                            <button id="break-increment" onClick={this.clickControls} value="breakIncrement"><img src={up}/></button>
-                            <div id="break-length">{this.state.breakLength}</div>
-                            <button id="break-decrement" onClick={this.clickControls} value="breakDecrement"><img src={down}/></button>
-                        </div>
-                    </div>
-                    <div id="session-label">
-                        <div id="session-head">
-                            <h3>Session</h3>
-                        </div>
-                        <div id="session-controls">
-                            <button id="session-increment" onClick={this.clickControls} value="sessionIncrement"><img src={up}/></button>
-                            <div id="session-length">{this.state.sessionLength}</div>
-                            <button id="session-decrement" onClick={this.clickControls} value="sessionDecrement"><img src={down}/></button>
-                        </div>
-                    </div>
-                </div>
-                <div id="clock">
-                    <h4 id="timer-label">Session</h4>
-                    <div id="time-left">
+    startStop() {
+        this.setState(play : !this.state.play)
+    }
 
-                        25:00
-                    </div>
-                    <div id="clock-controls">
-                        <div id="start_stop">
-                            <button id="play"><img src={play}/></button>
-                            <button id="pause"><img src={pause}/></button>
+    seconds() {
+        if () 
+            let sec = this.state.ss
+        if (sec === '00') {
+            sec = '59';
+        } else if (sec === '01') {
+            sec = '00';
+        } else {
+            sec = (parseInt(sec) - 1).toString();
+        }
+        this.setState({ss: sec});
+    }
+    reset() {
+        this.setState({breakLength: 5, sessionLength: 25, mm: 25, timerLabel: 'Session', play: false})
+    }
+
+    render() {
+        return (<div>
+            <div id="pomodoro">
+                <div id="head">
+                    <h1>Pomodoro clock</h1>
+                </div>
+                <div id="main">
+                    <div id="settings">
+                        <div id="settings-head">
+                            <h3>Settings:</h3>
                         </div>
-                        <button id="reset"><img src={reset}/></button>
+                        <div id="break-label">
+                            <div>
+                                <h3>Break</h3>
+                            </div>
+                            <div id="break-controls">
+                                <button id="break-increment" onClick={this.clickControls} value="breakIncrement"><img src={up}/></button>
+                                <div id="break-length">{this.state.breakLength}</div>
+                                <button id="break-decrement" onClick={this.clickControls} value="breakDecrement"><img src={down}/></button>
+                            </div>
+                        </div>
+                        <div id="session-label">
+                            <div id="session-head">
+                                <h3>{this.state.timerLabel}</h3>
+                            </div>
+                            <div id="session-controls">
+                                <button id="session-increment" onClick={this.clickControls} value="sessionIncrement"><img src={up}/></button>
+                                <div id="session-length">{this.state.sessionLength}</div>
+                                <button id="session-decrement" onClick={this.clickControls} value="sessionDecrement"><img src={down}/></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="clock">
+                        <h4 id="timer-label">Session</h4>
+                        <div id="time-left">{this.state.mm}:{this.state.ss}</div>
+                        <div id="clock-controls">
+                            <button id="start_stop" onClick={this.startStop}>Play/Pause</button>
+                            <button id="reset" onClick={this.reset}>Reset</button>
+                        </div>
                     </div>
                 </div>
             </div>
+            <h2 id="copyright">
+                by RubyLupus
+            </h2>
         </div>)
     }
 }
